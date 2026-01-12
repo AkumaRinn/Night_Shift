6cfb922cefe0393ad52ba78973141138c5e65452
Rinn_3301 <95349128+AkumaRinn@users.noreply.github.com>
2026-01-12 22:40:32 +0200

    Car Spawner implementation
    -added car spawner scene
    -dinamically create followpath3d with a car child
    -clear path once the car reached the end.
    -spawn another if the path is clear.

--- Interact_obj/Car/car.gd
+++ Interact_obj/Car/car.gd
@@ -2,7 +2,8 @@ extends RigidBody3D
 
 const CAR_SPEED := 10
 
-@onready var path_follow := get_parent().find_parent("PathFollow3D")
+@onready var path_follow: PathFollow3D = null
+
 var is_moving := true
 var is_waiting_for_player := false
 var is_activated := false
@@ -25,13 +26,19 @@ func interact(player):
 		player.fill_progress.value = 0
 	
 
-func _process(delta):
+func _physics_process(delta):
+	
+	if not path_follow:
+		return
+
 	if is_moving:
 		path_follow.progress += CAR_SPEED * delta
-		
-		if (path_follow.progress >= stop_point) && !is_activated: #if remove !is_activated, the car would stop again
+		global_transform = path_follow.global_transform
+		if (path_follow.progress >= stop_point) && !is_activated:
 			is_moving = false
 			is_waiting_for_player = true
 			
-	if path_follow.get_progress_ratio() >= 1:
+		
+	if path_follow.progress_ratio >= 0.9:
+		path_follow.queue_free()
 		queue_free()
