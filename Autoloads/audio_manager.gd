0fe2524296460b74c6690ae0c66afe4e908a60db
Rinn_3301 <95349128+AkumaRinn@users.noreply.github.com>
2026-01-28 21:29:17 +0200

    Clock in sound

--- Autoloads/audio_manager.gd
+++ Autoloads/audio_manager.gd
@@ -3,3 +3,4 @@ class_name AudioManagerNode extends Node
 @onready var lantern_sound = $LanternSound
 @onready var button_press_sound = $ButtonPressSound
 @onready var button_hover_sound = $ButtonHoverSound
+@onready var clock_in_sound = $ClockInSound
--- Interact_obj/PunchMachine/punch_machine.gd
+++ Interact_obj/PunchMachine/punch_machine.gd
@@ -3,6 +3,7 @@ extends Node3D
 var mission
 
 func interact(player):
+	AudioManager.clock_in_sound.play()
 	mission = player.player_mission
 	if mission.punch_mission_flag == 1:
 		mission.mission_finished()
